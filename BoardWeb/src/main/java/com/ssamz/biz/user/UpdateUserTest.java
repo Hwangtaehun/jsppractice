ba2f4038b3348dacb6b4d3c38859b7ccb8dbf32c
Hwangtaehun <r283@naver.com>
2024-08-27 15:51:35 +0900

    데이터 베이스 부분 업로드

--- BoardWeb/src/main/java/com/ssamz/biz/user/UpdateUserTest.java
+++ BoardWeb/src/main/java/com/ssamz/biz/user/UpdateUserTest.java
@@ -1,28 +1,21 @@
 package com.ssamz.biz.user;
 
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import com.ssamz.biz.common.JDBCUtil;
 
-public class InsertUserTest {
-	
+public class UpdateUserTest {
+
 	public static void main(String[] args) {
-		// 1. UserDAO 객체 생성한다.
+		// 1. UserDAO 객체를 생성한다.
 		UserDAO dao = new UserDAO();
 		
-		// 2. 회원 정보를 등록한다.
-		UserVO vo = new UserVO();
-		vo.setName("쌤즈");
-		vo.setRole("USER");
-		vo.setId("ssamz4");
-		vo.setPassword("ssamz123");
-		dao.insertUser(vo);
+		// 2. 회원 정보를 수정한다.
+		dao.updateUser("수정", "User", "ssamz3");
 		
-		
-		// 3. 목록 조회한다.
+		// 3. 목록을 조회한다.
 		dao.getUserList();
 		
 		/*
@@ -31,14 +24,16 @@ public class InsertUserTest {
 		 * try { conn = JDBCUtil.getConnection();
 		 * 
 		 * // JDBC 3단계 : Statement 생성 String sql =
-		 * "insert into users values(?, ?, ?, ?)"; stmt = conn.prepareStatement(sql);
+		 * "update users set name = ?, role = ? where id = ?"; stmt =
+		 * conn.prepareStatement(sql);
 		 * 
-		 * // JDBC 4단계 : SQL 전송 // ? 값 설정 stmt.setString(1, "ssamz3"); stmt.setString(2,
-		 * "ssamz123"); stmt.setString(3, "쌤즈"); stmt.setString(4, "ADMIN");
+		 * // JDBC 4단계 : SQL 전송 // ? 값 설정 stmt.setString(1, "수정"); stmt.setString(2,
+		 * "USER"); stmt.setString(3, "ssamz3");
 		 * 
 		 * // SQL 전송 int count = stmt.executeUpdate(); System.out.println(count +
 		 * "건 데이터 처리 성공!"); } catch (SQLException e) { // TODO Auto-generated catch
 		 * block e.printStackTrace(); } finally { JDBCUtil.close(stmt, conn); }
 		 */
 	}
+
 }
