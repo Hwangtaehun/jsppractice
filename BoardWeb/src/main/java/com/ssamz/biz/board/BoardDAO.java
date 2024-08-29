e241c32034f86ecb2e9824fb7d44488b82b293b4
Hwangtaehun <r283@naver.com>
2024-08-29 22:33:18 +0900

    화면 이동 부터 시작

--- BoardWeb/src/main/java/com/ssamz/biz/board/BoardDAO.java
+++ BoardWeb/src/main/java/com/ssamz/biz/board/BoardDAO.java
@@ -18,7 +18,7 @@ public class BoardDAO {
 	// SQL 명령어
 	private static String BOARD_INSERT = "insert into board(seq, title, writer, content) "
 			+ "values((select nvl(max(seq), 0) + 1 from board), ?, ?, ?)";
-	private static String BOARD_UPDATE = "update board set titel = ?, content = ? where seq = ?";
+	private static String BOARD_UPDATE = "update board set title = ?, content = ? where seq = ?";
 	private static String BOARD_DELETE = "delete board where seq = ?";
 	private static String BOARD_GET    = "select * from board where seq = ?";
 	private static String BOARD_LIST   = "select * from board order by seq desc";
